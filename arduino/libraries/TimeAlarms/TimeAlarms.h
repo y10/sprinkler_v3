d123161cfb30a1f98cf94aa736f64301d727de8e
y10 <serhiy.voytenko@gmail.com>
2024-07-09 08:38:18 -0400

    Increased number of posibble timers

--- arduino/libraries/TimeAlarms/TimeAlarms.h
+++ arduino/libraries/TimeAlarms/TimeAlarms.h
@@ -10,7 +10,7 @@
 #if defined(__AVR__)
 #define dtNBR_ALARMS 6   // max is 255
 #else
-#define dtNBR_ALARMS 12  // assume non-AVR has more memory
+#define dtNBR_ALARMS 15  // assume non-AVR has more memory
 #endif
 
 #define USE_SPECIALIST_METHODS  // define this for testing
